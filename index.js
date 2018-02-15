3456cc7274411e3fcbc19300281eb6468f1ec202
Mattias Sämskar <mattias.samskar@gmail.com>
2018-02-15 20:50:31 +0100

    Add plus and minus

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-var app = new Vue({
-  el: '#app',
+var app1 = new Vue({
+  el: '#app1',
   mounted: function () {
     this.update();
   },
@@ -9,22 +9,54 @@ var app = new Vue({
   },
   methods: {
     updateFirst: function (evt) {
-      this.first = getRandomInt();
+      this.first = getRandomInt(1, 9);
       evt.stopPropagation();
     },
     updateSecond: function (evt) {
-      this.second = getRandomInt();
+      this.second = getRandomInt(1 ,9);
       evt.stopPropagation();
     },
     update: function (evt) {
-      this.first = getRandomInt();
-      this.second = getRandomInt();
+      this.first = getRandomInt(1 ,9);
+      this.second = getRandomInt(1 ,9);
     }
   }
 });
 
-function getRandomInt() {
-  var min = 1;
-  var max = 9;
+var app2 = new Vue({
+  el: '#app2',
+  mounted: function () {
+    this.update();
+  },
+  data: {
+    first: 0,
+    second: 0
+  },
+  methods: {
+    update: function (evt) {
+      this.first = getRandomInt(100, 900);
+      this.second = getRandomInt(10 ,99);
+    }
+  }
+});
+
+var app3 = new Vue({
+  el: '#app3',
+  mounted: function () {
+    this.update();
+  },
+  data: {
+    first: 0,
+    second: 0
+  },
+  methods: {
+    update: function (evt) {
+      this.first = getRandomInt(100, 900);
+      this.second = getRandomInt(10 ,99);
+    }
+  }
+});
+
+function getRandomInt(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 }
